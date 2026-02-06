7f92eb08e94972baa357e1bbd9a69492d592b6bc
shashank-inventlix <shashank@inventlix.com>
2026-02-06 22:14:05 +0530

    Day13 completed login api

--- Day13/src/routes/auth.routes.js
+++ Day13/src/routes/auth.routes.js
@@ -3,7 +3,7 @@ const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 // require("dotenv").config();
 const authRouter = express.Router();
-
+const crypto = require("crypto");
 /**
  * @route POST /api/auth/register
  * @desc Register a new user
@@ -17,14 +17,16 @@ authRouter.post("/register", async (req, res) => {
   const isUserAlreadyExits = await userModel.findOne({ email });
 
   if (isUserAlreadyExits) {
-    return res.status(400).json({
+    return res.status(409).json({
       message: "User already exists",
     });
   }
 
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
   const user = await userModel.create({
     email,
-    password,
+    password: hash,
     name,
   });
 
@@ -45,5 +47,45 @@ authRouter.post("/register", async (req, res) => {
     token,
   });
 });
+
+
+/**
+ * @route POST /api/auth/login
+ * @desc Login user and return JWT token
+ * @access Public
+ */
+authRouter.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+
+  // Find user by email
+  const user = await userModel.findOne({email});
+
+  if(!user){
+    return res.status(404).json({
+      message : "User not found",
+    })
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  if(user.password !== hash){
+    return res.status(401).json({
+      message : "Invalid credentials",
+    })
+  }
+
+  const token = jwt.sign({
+    id : user._id,
+  },process.env.JWT_SECRET)
+
+  res.cookie("jwt_token", token);
+
+  res.json({
+    message : "Login successful",
+    user,
+    token,
+  })
+
+})
 
 module.exports = authRouter;
